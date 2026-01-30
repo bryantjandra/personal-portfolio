8a6a28712693e35f097e7a1292a0535336b1c782
bryantjandra <tjandra.benedict@gmail.com>
2026-01-30 14:01:16 +0800

    feat: initialize data layer and typescript interfaces

---
+++ src/data/index.ts
@@ -0,0 +1,31 @@
+import type { WorkExperience, Project } from "../types";
+
+export const workExperience: WorkExperience[] = [
+  {
+    logo: "/tiktokLogo.jpg",
+    company: "TikTok",
+    role: "Software Engineer Intern",
+    date: "2025",
+  },
+  {
+    logo: "/wsaLogo.jpg",
+    company: "WSAudiology",
+    role: "Backend Engineer Intern",
+    date: "2024",
+  },
+  {
+    logo: "/mulaiCodingLogo.png",
+    company: "Mulai Coding",
+    role: "Software Engineer Intern",
+    date: "2023",
+  },
+];
+
+export const projects: Project[] = [
+  {
+    logo: "/youtubeLogo.png",
+    title: "juniorSWE",
+    description: "trying to make people laugh",
+    url: "https://www.youtube.com/@juniorSWE",
+  },
+];
---
+++ src/types/index.ts
@@ -0,0 +1,13 @@
+export interface WorkExperience {
+  logo: string;
+  company: string;
+  role: string;
+  date: string;
+}
+
+export interface Project {
+  logo: string;
+  title: string;
+  description: string;
+  url: string;
+}
