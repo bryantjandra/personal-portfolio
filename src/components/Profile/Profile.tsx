941c82435c21885452e3d69080bc2bc7255836c4
bryantjandra <tjandra.benedict@gmail.com>
2026-03-31 14:53:25 +0800

    feat: add education line

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -8,11 +8,24 @@ export default function Profile() {
       <div className={styles.container}>
         <img src="/linkedinHeadshot.jpeg" className={styles.image} />
         <div className={styles.subContainer}>
-          <p className={styles.name}>Bryan Tjandra</p>
+          <p className={styles.name}>Benedict Bryan Tjandra</p>
           <p className={styles.description}>
-            I am a software engineer (ai + fullstack) currently building
+            • I am a software engineer (ai + fullstack) currently building
             ai-native solutions at TikTok.
           </p>
+          <p className={styles.description}>
+            • I graduated from the {""}
+            <a
+              className={styles.underlinedText}
+              href="https://www.comp.nus.edu.sg/bytes/nus-computing-clinches-1st-spot-in-asia-and-4th-worldwide/"
+              target="__blank"
+            >
+              National University of Singapore
+            </a>{" "}
+            {""} with a Bachelor's in Computer Science where I was ranked top 5%
+            of my entire cohort.
+          </p>
+
           <div className={styles.logos}>
             <a
               href="https://github.com/bryantjandra"
